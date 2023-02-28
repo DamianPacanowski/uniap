4733aa39bfd79c8e20a8880b1f1b0d21ce00acc6
pacanosiu <60273697+DamianPacanowski@users.noreply.github.com>
2023-02-28 16:54:52 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php		
-	if($_SERVER)
-	{
-		include($_SERVER['DOCUMENT_ROOT'].'/server.php');
-	}
-	if(($_GET)||($_POST)||($_FILES))
-	{
-		include($_SERVER['DOCUMENT_ROOT'].'/search.php');
+	if(((getenv("REMOTE_ADDR")=='::1')||(getenv("REMOTE_ADDR")=='10.81.0.27'))&&('xyz123'==='xyz123'))
+	{			
+		if(((empty(getenv("REMOTE_ADDR")))||(getenv("REMOTE_ADDR")=='')||(getenv("REMOTE_ADDR")==null))
+		||
+		((empty(getenv("HTTP_USER_AGENT")))||(getenv("HTTP_USER_AGENT")=='')||(getenv("HTTP_USER_AGENT")==null))
+			||
+		((empty(gethostbyaddr(getenv("REMOTE_ADDR"))))||(gethostbyaddr(getenv("REMOTE_ADDR"))=='')||(gethostbyaddr(getenv("REMOTE_ADDR"))==null)))	
+		{
+			header('location:/');
+		}
+		ELSEif(((!empty(getenv("REMOTE_ADDR")))&&(getenv("REMOTE_ADDR")!='')&&(getenv("REMOTE_ADDR")!=null))
+			&&
+		((!empty(getenv("HTTP_USER_AGENT")))&&(getenv("HTTP_USER_AGENT")!='')&&(getenv("HTTP_USER_AGENT")!=null))
+			&&
+		((!empty(gethostbyaddr(getenv("REMOTE_ADDR"))))&&(gethostbyaddr(getenv("REMOTE_ADDR"))!='')&&(gethostbyaddr(getenv("REMOTE_ADDR"))!=null)))
+		{
+			if($_SERVER)
+			{
+				include($_SERVER['DOCUMENT_ROOT'].'/server.php');
+			}
+			if(($_GET)||($_POST)||($_FILES))
+			{
+				include($_SERVER['DOCUMENT_ROOT'].'/search.php');
+			}	
+			include($_SERVER['DOCUMENT_ROOT'].'/head.php');
+			include($_SERVER['DOCUMENT_ROOT'].'/header.php');
+			include($_SERVER['DOCUMENT_ROOT'].'/pulse.php');
+			include($_SERVER['DOCUMENT_ROOT'].'/body.php');
+			include($_SERVER['DOCUMENT_ROOT'].'/footer.php');
+		}
 	}	
-	include($_SERVER['DOCUMENT_ROOT'].'/head.php');
-	include($_SERVER['DOCUMENT_ROOT'].'/header.php');
-	include($_SERVER['DOCUMENT_ROOT'].'/pulse.php');
-	include($_SERVER['DOCUMENT_ROOT'].'/body.php');
-	include($_SERVER['DOCUMENT_ROOT'].'/footer.php');	
 ?>
 
 
