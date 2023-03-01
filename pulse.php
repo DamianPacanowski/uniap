9db2c5bfe439e837c6fcd067534395054a44b788
pacanosiu <60273697+DamianPacanowski@users.noreply.github.com>
2023-03-01 04:25:52 +0100

    Delete pulse.php

--- pulse.php
+++
@@ -1,9 +0,0 @@
-<div style="text-align:center;">
-	<main>
-		<div class="atom">
-			<div class="electron"></div>
-			<div class="electron-alpha"></div>
-			<div class="electron-omega"></div>
-		</div>
-	</main>
-</div>
